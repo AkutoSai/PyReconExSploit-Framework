c6fb5bcc10b330c7515667aaeb9596ed44f1593d
Divyansh Mitra <36693722+AkutoSai@users.noreply.github.com>
2020-04-10 19:36:14 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,61 +8,12 @@ setup(
     version='0.0.3.5',
     author='Akuto Sai',
     description='Recon & Explitation Framework',
-    long_description="""# Installation Instructions:
-
-
-
-In order to use `search` command you must install the searchsploit binary! from {https://www.exploit-db.com/searchsploit/#install}
-
-## Building 
-```
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install exploitdb netcat nmap perl php
-pip3 install babysploit
-git clone https://github.com/AkutoSai/PyReconExSploit
-cd PyReconExSploit/
-python3 setup.py install
-pyreconexsploit
-```
-
-# Getting Started:
-
-#### Setting Configuration Values:
-
-PyReconExSploit uses ConfigParser in order to write and read configuration. Your config file is automatically
-generated and located at `./babysploit/config/config.cfg`. You can manually change configuration settings
-by opening up the file and editing with a text editor or you can use the set command to set a new value for
-a key. Use the set command like so:
-```
-set rhost
->> Enter Value For rhost: 10
->> Config Key Saved!
-```
-
-If before running this command the rhost key had a value of 80, the rhost key after running this command has a
-value of 10. You can also add configuration variables to the config by using the set command with a new key after it
-like so:
-```
-set newkey
->> Enter Value For newkey: hello
->> Config Key Saved!
-```
-
-Before running this there was no key named "newkey". After running this you will have a key named "newkey" in your config
-until you use the `reset` command which resets the saved configuration.
-
-#### Running A Tool
-
-In order to run a tool all you have to do is enter the name of the tool into pyreconexsploit. You can use the `tools` command
-to display a menu with all the currently available tools.
-
-""",
     long_description_content_type='text/markdown',
-    url='',
+    url='https://github.com/AkutoSai/PyReconExSploit',
     packages=['pyreconexsploit'],
     install_requires=[
         'netifaces',
+        'babysploit',
         'urllib3',
         'humanfriendly',
         'terminaltables',
